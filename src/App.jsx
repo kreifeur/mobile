c36007b1ac8ac2c065d28e0d4ed9386d173055ff
kreifeur <kreifeur669@gmail.com>
2023-07-28 19:48:29 +0100

    xx

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <div className="w-[100%] h-screen text-sm flex flex-col">
-      <div className="w-[100%] h-[5%] bg-blue-500 flex items-center ">App mobile </div>
+      <div className="w-[100%] h-[10%] bg-blue-500 flex items-center text-xl text-whire font-bold">KREIFEUR App mobile </div>
       <BrowserRouter>
 
 
           {/* les routes */}
           <Routes>
           <Route path="/mobile/" element={<Home />}></Route>
-            <Route path="/etat" element={<Etat />}></Route>
-            <Route path="/sell" element={<Sell />}></Route>
+            <Route path="/mobile/etat" element={<Etat />}></Route>
+            <Route path="/mobile/sell" element={<Sell />}></Route>
           </Routes>
 
 
